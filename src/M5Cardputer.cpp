add8b6f524ee2818aef9b074df4b23d6edfaa60b
sean <1026025056@qq.com>
2025-03-07 14:32:56 +0800

    fix button reference issues, use M5.getButton method

--- src/M5Cardputer.cpp
+++ src/M5Cardputer.cpp
@@ -4,7 +4,8 @@ using namespace m5;
 
 M5_CARDPUTER M5Cardputer;
 
-void M5_CARDPUTER::begin(bool enableKeyboard) {
+void M5_CARDPUTER::begin(bool enableKeyboard)
+{
     M5.begin();
     _enableKeyboard = enableKeyboard;
     if (enableKeyboard) {
@@ -12,7 +13,8 @@ void M5_CARDPUTER::begin(bool enableKeyboard) {
     }
 }
 
-void M5_CARDPUTER::begin(m5::M5Unified::config_t cfg, bool enableKeyboard) {
+void M5_CARDPUTER::begin(m5::M5Unified::config_t cfg, bool enableKeyboard)
+{
     M5.begin(cfg);
     _enableKeyboard = enableKeyboard;
     if (enableKeyboard) {
@@ -20,12 +22,11 @@ void M5_CARDPUTER::begin(m5::M5Unified::config_t cfg, bool enableKeyboard) {
     }
 }
 
-void M5_CARDPUTER::update(void) {
+void M5_CARDPUTER::update(void)
+{
     M5.update();
     if (_enableKeyboard) {
         Keyboard.updateKeyList();
         Keyboard.updateKeysState();
     }
 }
-
-
--- src/M5Cardputer.h
+++ src/M5Cardputer.h
@@ -6,11 +6,11 @@
 
 namespace m5 {
 class M5_CARDPUTER {
-   private:
+private:
     /* data */
     bool _enableKeyboard;
 
-   public:
+public:
     void begin(bool enableKeyboard = true);
     void begin(m5::M5Unified::config_t cfg, bool enableKeyboard = true);
 
@@ -19,7 +19,7 @@ class M5_CARDPUTER {
     Power_Class &Power     = M5.Power;
     Speaker_Class &Speaker = M5.Speaker;
     Mic_Class &Mic         = M5.Mic;
-    Button_Class &BtnA     = M5.BtnA;
+    Button_Class &BtnA     = M5.getButton(0);
 
     Keyboard_Class Keyboard = Keyboard_Class();
     /// for internal I2C device
